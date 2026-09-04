c88c99399cd467695d9a950a9104b562af5a51a2
Dan Mani Binu <dan@Dans-MacBook-Air.local>
2026-09-04 05:40:10 +0530

    dctcp-private-priority-websearch

---
+++ obm-sim-swift-websearch-priority-private/bash_clear_logs.sh
@@ -0,0 +1,2 @@
+rm -rf net-sim-*/logs/*
+rm -rf net-sim-*/prev_logs/all_logs/*
---
+++ obm-sim-swift-websearch-priority-private/run_all.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+mkdir -p junk_logs
+
+rm -rf net-sim-abm/logs && mkdir -p logs
+rm -rf net-sim-obm/logs && mkdir -p logs
+rm -rf net-sim-dt/logs && mkdir -p logs
+rm -rf net-sim-occamy/logs && mkdir -p logs
+rm -rf net-sim-credence/logs && mkdir -p logs
+rm -rf net-sim-lqd/logs && mkdir -p logs
+rm -rf net-sim-spreal/logs && mkdir -p logs
+rm -rf net-sim-lqd-ideal/logs && mkdir -p logs
+
+mkdir -p net-sim-abm/logs
+mkdir -p net-sim-obm/logs
+mkdir -p net-sim-dt/logs
+mkdir -p net-sim-occamy/logs
+mkdir -p net-sim-credence/logs
+mkdir -p net-sim-lqd/logs
+mkdir -p net-sim-spreal/logs
+mkdir -p net-sim-lqd-ideal/logs
+
+
+mkdir -p net-sim-abm/prev_logs/all_logs
+mkdir -p net-sim-obm/prev_logs/all_logs
+mkdir -p net-sim-dt/prev_logs/all_logs
+mkdir -p net-sim-occamy/prev_logs/all_logs
+mkdir -p net-sim-credence/prev_logs/all_logs
+mkdir -p net-sim-lqd/prev_logs/all_logs
+mkdir -p net-sim-spreal/prev_logs/all_logs
+mkdir -p net-sim-lqd-ideal/prev_logs/all_logs
+
+touch stats_abm.txt
+touch stats_obm.txt
+touch stats_dt.txt
+touch stats_credence.txt
+touch stats_lqd.txt
+touch stats_spreal.txt
+touch stats_lqd_ideal.txt
+touch stats_occamy.txt
+
+taskset -c 0 bash run_abm.sh        > junk_logs/abm.txt        2>&1 &
+taskset -c 1 bash run_credence.sh   > junk_logs/credence.txt   2>&1 &
+taskset -c 2 bash run_dt.sh         > junk_logs/dt.txt         2>&1 &
+taskset -c 3 bash run_lqd_ideal.sh  > junk_logs/lqd_ideal.txt  2>&1 &
+taskset -c 4 bash run_lqd_spreal.sh > junk_logs/lqd_spreal.txt 2>&1 &
+taskset -c 5 bash run_lqd.sh        > junk_logs/lqd.txt        2>&1 &
+taskset -c 6 bash run_obm.sh        > junk_logs/obm.txt        2>&1 &
+taskset -c 7 bash run_occamy.sh     > junk_logs/occamy.txt     2>&1 &
+
+wait
+
+echo "All simulations finished."
