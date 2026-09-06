8615448611b3cce15911a3a51a6bf1efacd35127
Dan Mani Binu <dan@Dans-MacBook-Air.local>
2026-09-06 06:29:44 +0530

    fixed run_all

--- obm-sim-dctcp-incast-n-priority-shared/run_all.sh
+++ obm-sim-dctcp-incast-n-priority-shared/run_all.sh
@@ -39,14 +39,23 @@ touch stats_spreal.txt
 touch stats_lqd_ideal.txt
 touch stats_occamy.txt
 
-taskset -c 0 bash run_abm.sh        > junk_logs/abm.txt        2>&1 &
-taskset -c 1 bash run_credence.sh   > junk_logs/credence.txt   2>&1 &
-taskset -c 2 bash run_dt.sh         > junk_logs/dt.txt         2>&1 &
-taskset -c 3 bash run_lqd_ideal.sh  > junk_logs/lqd_ideal.txt  2>&1 &
-taskset -c 4 bash run_lqd_spreal.sh > junk_logs/lqd_spreal.txt 2>&1 &
-taskset -c 5 bash run_lqd.sh        > junk_logs/lqd.txt        2>&1 &
-taskset -c 6 bash run_obm.sh        > junk_logs/obm.txt        2>&1 &
-taskset -c 7 bash run_occamy.sh     > junk_logs/occamy.txt     2>&1 &
+export PYTHONUNBUFFERED=1
+
+taskset -c 0 bash run_abm.sh        > junk_logs/abm.txt &
+
+taskset -c 1 bash run_credence.sh   > junk_logs/credence.txt &
+
+taskset -c 2 bash run_dt.sh         > junk_logs/dt.txt &
+
+taskset -c 3 bash run_lqd_ideal.sh  > junk_logs/lqd_ideal.txt &
+
+taskset -c 4 bash run_spreal.sh     > junk_logs/lqd_spreal.txt &
+
+taskset -c 5 bash run_lqd.sh        > junk_logs/lqd.txt &
+
+taskset -c 6 bash run_obm.sh        > junk_logs/obm.txt &
+
+taskset -c 7 bash run_occamy.sh     > junk_logs/occamy.txt &
 
 wait
 
---
+++ run.sh
@@ -0,0 +1,8 @@
+cd obm-sim-dctcp-incast-n-priority-private
+bash run_all.sh
+cd ..
+
+cd obm-sim-dctcp-incast-n-priority-shared
+bash run_all.sh
+cd ..
+
