8ec20b8e5f600442638d897051f07e684c0fd665
Dan Mani Binu <dan@Dans-MacBook-Air.local>
2026-09-08 13:30:15 +0530

    fixed spreal

--- switch-sim-private/bash_prvt.sh
+++ switch-sim-private/bash_prvt.sh
@@ -19,7 +19,7 @@ for i in ${!burst_percent[@]}; do
     taskset -c $((7*i+1)) /bin/python 'algo_dt.py' ${burst_percent[$i]} $incast_value $round_values $alpha | tee  master/dt_logs/output_dt_${burst_percent[$i]}_${incast_value}_${round_values}_${alpha}.txt &
     # taskset -c $((7*i+2)) /bin/python 'LQD/algo_obm_latency.py' ${burst_percent[$i]} $incast_value $round_values 10| tee  LQD/master/obm_logs/output_obm_${burst_percent[$i]}_${incast_value}_${round_values}.txt &
     taskset -c $((7*i+3)) /bin/python 'algo_obm_bufferdrop.py' ${burst_percent[$i]} $incast_value $round_values | tee  master/obm_logs/output_obm_${burst_percent[$i]}_${incast_value}_${round_values}.txt &
-    taskset -c $((7*i+4)) /bin/python 'algo_spreal.py' ${burst_percent[$i]} $incast_value $round_values | tee  master/spreal_logs/output_obm_${burst_percent[$i]}_${incast_value}_${round_values}.txt &
+    taskset -c $((7*i+4)) /bin/python 'algo_spreal.py' ${burst_percent[$i]} $incast_value $round_values | tee  master/spreal_logs/output_spreal_${burst_percent[$i]}_${incast_value}_${round_values}.txt &
     taskset -c $((7*i+5)) /bin/python 'algo_occamy.py' ${burst_percent[$i]} $incast_value $round_values 16 | tee master/occamy_logs/output_occamy_${burst_percent[$i]}_${incast_value}_${round_values}_${alpha}.txt &
     taskset -c $((7*i+6)) /bin/python 'algo_optimal_attmpt.py' ${burst_percent[$i]} $incast_value $round_values  | tee  master/optimal_logs/output_optimal_${burst_percent[$i]}_${incast_value}_${round_values}.txt &
     taskset -c $((7*i+2)) /bin/python 'algo_credence.py' ${burst_percent[$i]} $incast_value $round_values 16 
